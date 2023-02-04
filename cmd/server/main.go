3f55fc9ff01e952a3b4f7a10aa1bac3d8842a6f0
marianStykhun <marian_s@webdevelop.pro>
2023-02-04 16:19:58 +0200

    submodules

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,51 +1,23 @@
 package main
 
 import (
-	"context"
-
 	db "github.com/Nav1Cr0ss/s-auth/internal/adapters/repository"
 	repository "github.com/Nav1Cr0ss/s-auth/internal/adapters/repository/sqlc"
 	oapi "github.com/Nav1Cr0ss/s-auth/internal/api"
 	"github.com/Nav1Cr0ss/s-auth/internal/app"
 	"github.com/Nav1Cr0ss/s-auth/internal/config"
 	handler "github.com/Nav1Cr0ss/s-auth/internal/ports/http"
-	"github.com/Nav1Cr0ss/s-auth/pkg/logger"
 	_ "github.com/lib/pq"
 	"go.uber.org/fx"
 )
 
 func main() {
-	//log := logger.NewLogger()
-	//
-	//conf, err := app.NewConfiguration(log)
-	//if err != nil {
-	//	fmt.Println(err)
-	//}
-	//
-	//db, err := sql.Open("postgres", fmt.Sprintf("postgresql://%s:0608@localhost:5436/s-auth?sslmode=disable", conf.Db.UserName))
-	//if err != nil {
-	//	return
-	//}
-	//
-	//repo := repository.New(db)
-	//
-	//application := app.NewApplication(repo, log)
-	//
-	//h := handler.NewHandler(log, application)
-	//
-	//srv, err := api2.NewServer(h, Bearer{})
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//if err := http.ListenAndServe(":8080", srv); err != nil {
-	//	log.Fatal(err)
-	//}
-
 	fx.New(
 		fx.Provide(
-			logger.NewLogger,
 			config.NewConfiguration,
-
+			func(cfg *config.Config) *logger.Logger {
+				return logger.NewLogger(cfg.Debug)
+			},
 			fx.Annotate(
 				db.NewDB,
 				fx.As(new(repository.DBTX)),
@@ -69,17 +41,6 @@ func main() {
 		),
 		fx.Invoke(
 			handler.StartServer,
-			func(lc fx.Lifecycle, log *logger.Logger, cfg *config.Config) {
-				lc.Append(
-					fx.Hook{
-						OnStart: func(ctx context.Context) error {
-							log.Info("failed to fetch URL")
-
-							return nil
-						},
-					},
-				)
-			},
 		),
 	).Run()
 }
--- internal/ports/http/server.go
+++ internal/ports/http/server.go
@@ -1,32 +1,16 @@
 package handler
 
 import (
-	"context"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/Nav1Cr0ss/s-auth/internal/api"
-	"go.uber.org/fx"
+	"github.com/Nav1Cr0ss/s-auth/internal/config"
 )
 
-func StartServer(lc fx.Lifecycle, srv *api.Server) {
-	lc.Append(
-		fx.Hook{
-			OnStart: func(ctx context.Context) error {
-				if err := http.ListenAndServe(":8080", srv); err != nil {
-					log.Fatal(err)
-				}
-
-				return nil
-			},
-			//OnStop: func(ctx context.Context) error {
-			//	err := srv.Echo.Shutdown(ctx)
-			//	if err != nil {
-			//		srv.log.Info().Err(err).Msg("couldn't stop server")
-			//	}
-			//
-			//	return nil
-			//},
-		},
-	)
+func StartServer(srv *api.Server, cfg *config.Config) {
+	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", cfg.Host, cfg.Port), srv); err != nil {
+		log.Fatal(err)
+	}
 }
